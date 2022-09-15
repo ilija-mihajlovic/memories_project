9a2f27d20b992163eb29541d4863e871a8c0b14d
Ilija Mihajlovic <iilija879a@gmail.com>
2022-09-15 20:18:03 +0200

    crud

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,35 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from 'react-file-base64'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import useStyles from './styles'
-import { createPost } from '../../actions/posts'
+import { createPost, updatePost } from '../../actions/posts'
 
-const Form = () => {
+// GET THE CURRENT ID OF THE POST
+
+const Form = ({ currentId, setCurrentId }) => {
+  const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
   const dispatch = useDispatch()
   const [postData, setPostData] = useState({
     creator: '', 
     title: '', 
     message: '', 
-    selectedFile: ''
+    selectedFile: '',
+    tags: ''
   })
   const classes = useStyles()
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    dispatch(createPost(postData))
-  }
-
+  useEffect(() => {
+    if(post){
+      setPostData(post)
+    }
+  }, [post])
+  
   const clear = () => {
-
+    setCurrentId(null)
+    setPostData({ creator: '', title: '', message: '', selectedFile: '', tags: ''})
   }
+
+  const handleSubmit = (e) => {
+
+    if(currentId){
+      dispatch(updatePost(currentId, postData))
+    }else{
+      dispatch(createPost(postData))
+    }
+    clear()
+    
+  }
+
 
   return (
     <Paper className={classes.paper}>
         <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-            <Typography variant="h6">Creating a Memory</Typography>
+            <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
             <TextField
               name='creator'
               variant='outlined'
@@ -54,13 +71,11 @@ const Form = () => {
               value={postData.message}
               onChange={(e) => setPostData({ ...postData, message: e.target.value })}        
             />
-            <TextField
-              name='tags'
-              variant='outlined'
-              label="Tags"
-              fullWidth
-              value={postData.tags}
-              onChange={(e) => setPostData({ ...postData, tags: e.target.value })}        
+            <TextField name="tags" 
+              variant="outlined" 
+              label="Tags (coma separated)" 
+              fullWidth value={postData.tags} 
+              onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} 
             />
             <div className={classes.fileInput}>
                 <FileBase
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -2,8 +2,13 @@ export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
             return action.payload
+        case 'DELETE':
+            return posts.filter((post) => post._id !== action.payload)
         case 'CREATE':
             return [ ...posts, action.payload] 
+        case 'UPDATE':
+        case 'LIKE':
+            return [ posts.map((post) => post._id === action.payload._id ? action.payload : post)]
         default:
             return posts;
     }
